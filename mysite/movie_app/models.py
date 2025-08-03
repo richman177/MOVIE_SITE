652594c12be7d11ff498ebc0735bcfe07dd548f2
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-03 11:06:31 +0600

    Update models.py
    hj

--- mysite/movie_app/models.py
+++ mysite/movie_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models 
-from django.contrib.auth.models import  AbstractUser
+from django.contrib.auth.models import  AbstractUser 
 from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db.models import PositiveSmallIntegerField
